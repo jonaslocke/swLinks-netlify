66fa7572c968b175e4d65977fa9e828f2f64bef4
Jonas Antunes <jonas.lima@grupogaia.com.br>
2020-12-21 09:40:25 -0300

    set Environment variables

--- src/api.js
+++ src/api.js
@@ -4,19 +4,16 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv/config");
 
 //MODELS
 const players = require("./routes/players");
 const links = require("./routes/links");
 
 //DB CONNECTION
-mongoose.connect(
-  "mongodb+srv://admin:c4llm34n0w@swlinks.9x4u4.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
-  () => {
-    console.log("CONNECTED");
-  }
-);
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("CONNECTED");
+});
 
 //MIDDLEWARES
 app.use(cors());
