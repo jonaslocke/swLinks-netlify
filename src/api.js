80de67483c4d730edf03f7d7701e50ea16e2fdf4
Jonas Antunes <jonas.lima@grupogaia.com.br>
2020-12-21 07:27:58 -0300

    dotenv issue

--- src/api.js
+++ src/api.js
@@ -4,16 +4,19 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv/config");
 
 //MODELS
 const players = require("./routes/players");
 const links = require("./routes/links");
 
 //DB CONNECTION
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-  console.log("CONNECTED");
-});
+mongoose.connect(
+  "mongodb+srv://admin:c4llm34n0w@swlinks.9x4u4.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("CONNECTED");
+  }
+);
 
 //MIDDLEWARES
 app.use(cors());
